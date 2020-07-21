6ead71ea63e55614046e6570a7095cf12ba3c607
hentoe <hendriktoedter@gmx.de>
2020-07-21 17:28:08 +0200

    Added css styles

--- website/settings.py
+++ website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["hentoe.pythonanywhere.com"]
+ALLOWED_HOSTS = ["localhost", "hentoe.pythonanywhere.com"]
 
 
 # Application definition
