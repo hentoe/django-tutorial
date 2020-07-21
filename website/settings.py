732edb3f62128bcdc19272d2bb72798e4782a096
hentoe <hendriktoedter@gmx.de>
2020-07-21 19:45:39 +0200

    Added post details

--- website/settings.py
+++ website/settings.py
@@ -22,7 +22,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
